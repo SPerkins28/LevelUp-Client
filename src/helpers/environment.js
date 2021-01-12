8f153a5cfd59a435b54d1b1e7b3b7866f2149b08
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2021-01-12 11:51:47 -0500

    changed case to load localhost unitl heroku server issue is addressed

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,8 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:4321';
         break;
     case 'sgp-level-up-client.herokuapp.com':
-        APIURL = 'http://sgp-level-up-client.herokuapp.com'
+        // APIURL = 'https://sgp-level-up-server.herokuapp.com'
+        APIURL = 'http://localhost:4321';
 }
 
 export default APIURL;
