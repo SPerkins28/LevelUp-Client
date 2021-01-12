1e3e3156e4f5ab76ced002bc086c33e2a98f8aa3
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2021-01-12 15:05:15 -0500

    testing new APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,11 +2,11 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:4321';
+        APIURL = 'https://localhost:4321';
         break;
     case 'sgp-level-up-client.herokuapp.com':
         // APIURL = 'https://sgp-level-up-server.herokuapp.com'
-        APIURL = 'http://localhost:4321';
+        APIURL = 'https://localhost:4321';
 }
 
 export default APIURL;
